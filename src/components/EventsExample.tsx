8917731f71a375837703522f8b694052c3ae142e
Adelheid483 <adelheid0106@gmail.com>
2021-06-13 19:42:10 +0300

    feat: add drag and drop events

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -3,12 +3,13 @@ import React, {FC, useState} from 'react';
 export const EventsExample: FC = () => {
 
     const [value, setValue] = useState<string>('');
+    const [isDrag, setIsDrag] = useState<boolean>(false);
 
     const styleDrag = {
         width: '400px',
         height: '150px',
         border: '2px solid purple',
-        background: 'pink',
+        background: isDrag ? 'pink' : 'blue',
         margin: '10px',
     };
 
@@ -24,12 +25,35 @@ export const EventsExample: FC = () => {
         console.log('Drag start');
     };
 
+    const dropHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDrag(() => false);
+        console.log('Stop drag');
+    };
+
+    // вышли за пределы дива
+    const leaveHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDrag(() => false);
+    };
+
+    // внутри дива
+    const overHandler = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDrag(() => true);
+    };
+
     return (
         <section>
             <input type="text" onChange={changeHandler} value={value}/>
             <button onClick={clickHandler}>Send to Log</button>
             <div style={styleDrag} draggable onDrag={dragHandler}> </div>
-            <div style={styleDrag}> </div>
+            <div
+                style={styleDrag}
+                onDrop={dropHandler}
+                onDragLeave={leaveHandler}
+                onDragOver={overHandler}
+            > </div>
         </section>
     );
 };
